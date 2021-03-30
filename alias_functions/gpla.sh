83d710439bb6a2faa0468a18e23d4d7b0cdf79a0
rafaelbdb <r.borjovsky@gmail.com>
2021-03-30 18:34:48 -0300

    gpla - Add git pull --all

---
+++ alias_functions/gpla.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Author: Diogo Alexsander Cavilha <diogocavilha@gmail.com>
+# Date:   04.01.2016
+#
+# git pull
+
+. ~/.fancy-git/random_messages.sh
+
+run() {
+    git pull --all
+
+    _fancygit_after_pulling_random_message
+}
